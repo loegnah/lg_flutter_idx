7cf3eb9d90efb43b5fb62b4a935ef23fe5a54b36
hangeol.park <hangeol.park@lge.com>
2024-07-24 15:46:23 +0900

    page1 complete

--- lib/page/page1.dart
+++ lib/page/page1.dart
@@ -8,34 +8,37 @@ class Page1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<IconData> iconList = [
+      Ionicons.camera_outline,
+      Ionicons.camera_reverse_outline,
+      Ionicons.image_outline,
+      Ionicons.videocam_outline,
+    ];
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             style: TextStyle(
               fontSize: 25,
               fontWeight: FontWeight.bold,
             ),
             '포토네컷',
           ),
-          Gap(10),
-          FadeIn(
-            duration: Duration(seconds: 1),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Ionicons.camera_outline),
-                Icon(Ionicons.camera_reverse_outline),
-                Icon(Ionicons.image_outline),
-                Icon(Ionicons.videocam_outline),
-              ],
-            ),
+          const Gap(10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: iconList.asMap().entries.map((e) {
+              return FadeInRight(
+                delay: Duration(milliseconds: e.key * 500),
+                child: Icon(e.value),
+              );
+            }).toList(),
           ),
-          Gap(10),
+          const Gap(10),
           ElevatedButton(
             onPressed: () {},
-            child: Text('시작하기'),
+            child: const Text('시작하기'),
           ),
         ],
       ),
--- lib/page/page2.dart
+++ lib/page/page2.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Page1 extends StatelessWidget {
-  const Page1({super.key});
+class Page2 extends StatefulWidget {
+  const Page2({super.key});
 
   @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold();
   }
 }
